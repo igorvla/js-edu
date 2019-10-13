a135681b8f1c79019f061ae82a0cf544c1f0faca
igorvla <16.may.1997@mail.ru>
2019-10-13 12:30:30 +0300

    init: start task js-edu

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
-/**
+/*
  * @param preferences - target student focus
  * @param knowsProgramming - if student can do programming and know basics
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
+    focus = 'family',
     knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+    config = { family: 4 }
+) {
+
+    var baseTime = 500; // для базовых навыков
+    var fullTime = 800; // для овладения языком
+    var coefficient = 1;
+
+    if (knowsProgramming == true) {
+        coefficient;
+    } else if (knowsProgramming == false) {
+        coefficient = ((fullTime + baseTime) / fullTime);
+    }
+
+    switch (focus) {
+        case 'family':
+            return Math.ceil(coefficient * (fullTime / config.family));
+            break;
+        case 'friends':
+            return Math.ceil(coefficient * (fullTime / config.friends));
+            break;
+        case 'normal_life':
+            return Math.ceil(coefficient * (fullTime / config.normal_life));
+            break;
+        case 'profession':
+            return Math.ceil(coefficient * (fullTime / config.profession));
+            break;
+        case 'carrier':
+            return Math.ceil(coefficient * (fullTime / config.carrier));
+            break;
+        case 'top_peformance':
+            return Math.ceil(coefficient * (fullTime / config.top_peformance));
+            break;
+    }
+};
